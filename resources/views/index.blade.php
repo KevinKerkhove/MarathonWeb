afa0ab396de2caccd55bde19246bb86a99507962
Kεяκнσνε Kενıп(C'εƨт Bεпɢ Bεпɢ) <kevin_kerkhove@ens.univ-artois.fr>
2019-12-18 17:30:32 +0100

    update series+ serie

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@ use App\Serie;
         @foreach($genres as $genre)
             <div>{{$genre['nom']}}</div>
             @foreach($genre->series as $s)
-                <a href="{{route('serie.show',$s->id)}}"><img src="{{$s->urlImage}}"></a>
+                <a href="{{route('serie.show',$s->id)}}"><img src=http://172.31.146.100/~dut19_groupe16"{{$s->urlImage}}"></a>
                     <p></br>{{$s->nom}}</br>{!!$s->resume!!}</p></br>
             @endforeach
         @endforeach
