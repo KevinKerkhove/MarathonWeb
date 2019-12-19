e6947f83b4df5540968c316783f7e2af80d0565c
Denis Dumont <dumontdd59@gmail.com>
2019-12-19 03:19:18 +0100

    reset

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,7 +11,6 @@ class MainController extends Controller
     public function index() {
         $series4View=Serie::orderBy('note','DESC')->limit(4)->get();
         $series4Here=Serie::orderBy('created_at','DESC')->limit(4)->get();
-        $FiveComments=Comment:orderBy()->limit(5)->get()
         $genres=Genre::all();
         return view("index",['series4View'=>$series4View,'series4Here'=>$series4Here,'genres'=>$genres]);
     }
