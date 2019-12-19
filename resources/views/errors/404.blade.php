d4dd167c2954f89b65648c8795f5323fc962b713
Denis Dumont <dumontdd59@gmail.com>
2019-12-19 15:38:10 +0100

    push du turfu

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+
+
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container_404">
+        <div class="content_404">
+            <div class="items_404">
+                <span id="erreur">erreur</span>
+                <span id="qcq">404</span>
+            </div>
+        </div>
+    </div>
+@endsection
